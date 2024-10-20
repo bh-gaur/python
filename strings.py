3f16dbcadd1c11344119e4a667ddd74a27ceb06b
Bhupender Gaur <150256375+bh-gaur@users.noreply.github.com>
2024-10-20 13:55:27 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,58 @@
+# print("hello \nworld") #print world in new line
+# print("hello \tworld") #print tab space in new line
+
+# str1 = "python"
+# str2 = "testing"
+
+# print(str1 + " " + str2) #concatenate two strings
+
+# str = "I am learning python"
+
+# print(str[0]) #print first character of string
+
+# print(str[0:]) #print complete string
+
+# print(str[:len(str)]) #It also print complete string 
+
+# print(str[-15:]) #print learning python using negative indexing
+
+# print(str.startswith("le")) #check string starts with substring(le)
+
+# print(str.endswith("on")) #check string ends with substring(on)
+
+#print(str.capitalize()) #capitaling first character of string
+
+# print(str.replace("am","bhola")) #replace "am" to "old" 
+
+# print(str.find("a")) #print the 1st index of 1st occurrences
+
+# print(str.count("am")) #count the occurrence of substring am
+
+# txt = "The best things in life are free!"
+# print("free" in txt) #check substring is in txt variable
+
+# txt = "The best things in life are free!"
+# print("expensive" not in txt) #check given substring is present or not in txt
+
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua.""" #create multiline string
+# print(a) #print multline string
+
+# txt = f"The price is {20 * 59} dollars"
+# print(txt)
+
+# a = "Hello, World!"
+# print(a.upper()) #print all character in upper string
+
+# print(a.lower()) #print all character in lower string
+
+                     ###WAP to input user's first name and print their length
+# name = input("enter your first name: ")
+# print("length of your name is", len(name))
+
+
+                     ###WAP to find the occurrence of '$' string
+# b = "Hi, $I am $ symbol $string"
+# print(b.count("$"))
