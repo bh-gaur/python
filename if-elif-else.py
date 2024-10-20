f2061c879b45120fe9d3dd9488f7954fe41dc273
Bhupender Gaur <150256375+bh-gaur@users.noreply.github.com>
2024-10-20 18:56:33 +0530

    Create if-elif-else.py

---
+++ if-elif-else.py
@@ -0,0 +1,41 @@
+                ###check number is even or odd
+# num = int(input("enter a number: "))
+
+# if(num%2 == 0):
+#     print("number is even")
+# elif(num%2 == 1):
+#     print("number is odd")
+
+# new = num % 2
+# if(new == 0):
+#     print("EVEN")
+# else:
+#     print("ODD")
+
+                ###WAP to find greatest of 4 numbers entered by user
+
+# a = int(input("enter first number: "))
+# b = int(input("enter second number: "))
+# c = int(input("enter third number: "))
+# d = int(input("enter fourth number: "))
+
+# if(a>=b and a>=c and a>=d):
+#     print("first number is largest:",a)
+# elif(b>=c and b>=d):
+#     print("second number is largest:",b)
+# elif(c>=d):
+#     print("third number is largest:",c)
+# else:
+#     print("fourth number is largest:",d)
+
+
+               ###WAP to program to check if number is a multiple of 7 or not
+
+# number = int(input("enter a number: "))
+
+# next = number % 7
+
+# if(next == 0):
+#     print("given number is multiple of seven")
+# else:
+#     print("given number is not multiple of seven")
