eeb9c99cb609031ac36bdca8cc5519263269b28f
Bhupender Gaur <150256375+bh-gaur@users.noreply.github.com>
2024-10-22 22:23:05 +0530

    Create counting.py

---
+++ counting.py
@@ -0,0 +1,17 @@
+#print numbers from 1 to 5 using while loop
+num = 1
+
+while num < 5:
+    print(num)
+    num +=1
+
+print("loop ended")
+
+#print numbers from 5 to 1 using while loop
+rev = 5
+
+while rev >= 1:
+    print(rev)
+    rev -= 1
+
+print("loop ended")
