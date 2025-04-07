cba721c89418a804d8ec22fabc272d6ec977fee5
bhola <gaurb8863@gmail.com>
2025-04-07 14:43:54 +0530

    add random.py

---
+++ random.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+# def generate_random_string_with_special_chars(length: int):
+#     characters = string.ascii_letters + string.digits + string.punctuation  # Letters, digits, and punctuation
+#     random_string = ''.join(random.choice(characters) for _ in range(length))
+#     return random_string
+
+# # Example: Generate a random string of length 12
+# random_string = generate_random_string_with_special_chars(12)
+# print(random_string)
+
+x = random.randint(1,100)
+print(x)
